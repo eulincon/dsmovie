ee73433f775b5932c78418d20543312c9169c1ee
lincon <linconsilva08@gmail.com>
2022-01-12 19:22:33 -0300

    Security config

---
+++ backend/src/main/kotlin/com/devsuperior/dsmovie/config/SecurityConfig.kt
@@ -0,0 +1,40 @@
+package com.devsuperior.dsmovie.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val env: Environment
+) : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        if (env.activeProfiles.equals("test")) {
+            http.headers().frameOptions().disable()
+        }
+        http.cors().and().csrf().disable()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.authorizeRequests().anyRequest().permitAll()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration().applyPermitDefaultValues()
+        configuration.allowedMethods = listOf("POST", "GET", "PUT", "DELETE", "OPTIONS")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
